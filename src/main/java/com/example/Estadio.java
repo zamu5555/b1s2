f0ffb2317edf5eef8af2e86ae08c48db8c9bfe4e
Samuel Diaz <samueld2604@gmail.com>
2025-07-15 10:30:29 -0500

    Adelanto ejercicio 1

---
+++ src/main/java/com/example/Estadio.java
@@ -0,0 +1,15 @@
+package com.example;
+
+class Estadio {
+
+    String nombre;
+    String ciudad;
+    int capacidad;
+
+    public void mostrarInformacion() {
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Ciudad: " + ciudad);
+    System.out.println("Capacidad: " + capacidad);
+
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,23 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-       
+        Estadio estadio1 = new Estadio();
+        Estadio estadio2 = new Estadio();
+
+
+        estadio1.nombre = "Estadio Metropolitano";
+        estadio1.ciudad = "Madrid";
+        estadio1.capacidad = 36000;
+
+        estadio2.nombre = "Estadio El Campín";
+        estadio2.ciudad = "Madrid";
+        estadio2.capacidad = 46692;
+
+
+
+       estadio1.mostrarInformacion();
+       System.out.println("----------------------------------------------------------------------------------------");
+       estadio2.mostrarInformacion();
     }
 }
 
